ec2562f3267a5660738043147f4b52af2ad63320
lennox <lennox@noreply.com>
2023-12-15 16:06:36 +0000

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Render Wiki Action
+
+Action to allow rendering of wiki contents from executable build artifacts.
+
+Useful if there are several executables in a repository which provide usage comments and documentation when executed. By specifying the wiki config yaml, any usage comments can be included in the markdown wiki.
+
+This action currently overwrites the contents of a wiki, it does not clean the folder first.
+
+> TODO:
+>
+> finish this readme
+>
+> provide option to clean folder
---
+++ render-wiki.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+set -Euo pipefail
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+  # script cleanup here
+}
+
+joinPaths() {
+  basePath=${1}
+  subPath=${2}
+  fullPath="${basePath:+$basePath/}$subPath"
+  printf "${fullPath}"
+}
+
+makeMarkdownFromCommand() {
+  printf "# $1\n\n"'```plaintext'"\n$($2)\n"'```'"\n"
+}
+
+### test inputs to remove ###
+
+INPUT_WIKI_PATH='wiki'
+INPUT_WIKI_CONFIG='./test/demo.yaml'
+
+#### start ####
+
+[ -z "${INPUT_WIKI_PATH-}" ] && echo "No wiki path given" && exit 1
+[ -z "${INPUT_WIKI_CONFIG-}" ] && echo "No wiki config yaml given" && exit 1
+[ ! -f "${INPUT_WIKI_CONFIG-}" ] && echo "No wiki config yaml found" && exit 1
+
+# creat wiki folder if it does not exist
+mkdir -p "${INPUT_WIKI_PATH}"
+
+#TODO: provide option to optionally clean folder
+#rm -rf "${INPUT_WIKI_PATH}"/*
+
+homeMD=$(joinPaths "${INPUT_WIKI_PATH}" "$(yq '.wiki.site.home' ${INPUT_WIKI_CONFIG})")
+
+title="$(yq '.wiki.site.title' ${INPUT_WIKI_CONFIG})"
+desc="$(yq '.wiki.site.description' ${INPUT_WIKI_CONFIG})"
+
+printf "\n# $title\n\n$desc\n" | tee ${homeMD}
+
+readarray pages < <(yq -o=j -I=0 ".wiki.pages[]" ${INPUT_WIKI_CONFIG})
+
+echo "---"
+
+index=0
+for page in "${pages[@]}"; do
+  name=$(echo "$page" | yq '.name' -)
+  nameMD=$(echo "${name}.md" | sed -r 's/(^|-|_| )([a-z])/\U\2/g')
+  pathMD=$(joinPaths "${INPUT_WIKI_PATH}" "${nameMD}")
+
+  # write heading to new markdown page
+  printf "# ${name}\n\n" | tee "${pathMD}"
+
+  # write link to home page
+  printf "\n[${name}](${nameMD})\n" | tee -a ${homeMD}
+
+  # get list of markdown renders for page
+  readarray renderRaw < <(yq -o=j -I=0 ".wiki.pages[${index}].render" ${INPUT_WIKI_CONFIG})
+  renderCSV=$(echo "[$renderRaw]" | tr -d "[]")
+  readarray -t -s 1 renderList < <(echo $renderCSV | awk -v FPAT='[^,]*|"[^"]*"' '{for (i=0;i<=NF;i++) print $i}')
+
+  echo "page: $name, ${#renderList[@]} items to render"
+
+  for renderItem in "${renderList[@]}"; do
+    if jq -e 'keys[0]' >/dev/null 2>&1 <<<"${renderItem}"; then
+      item=$(echo "${renderItem}" | jq 'keys[0]' | sed -e 's/^"//' -e 's/"$//')
+      command=$(echo "${renderItem}" | jq '.[keys_unsorted[0]]' | sed -e 's/^"//' -e 's/"$//')
+      #echo "itemName=$item command=$command"
+
+      makeMarkdownFromCommand "${item}" "${command}" | tee -a ${pathMD}
+    else
+      markdown=$(echo "${renderItem}" | sed -e 's/^"//' -e 's/"$//')
+      #echo "markdown=${markdown}"
+
+      printf "${markdown}"| tee -a ${pathMD}
+    fi
+  done
+
+  echo "---"
+  ((index++))
+done
